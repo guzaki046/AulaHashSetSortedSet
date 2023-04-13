f6f19f3b334cc587d1ac34ff045daeb249d83fe9
Gustavo Nozaki Soares <gnozakisoares@gmail.com>
2023-04-13 17:22:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+HashSet<string> set = new HashSet<string>();
+
+set.Add("TV");
+set.Add("Notebook");
+set.Add("Tablet");
+
+Console.WriteLine(set.Contains("Computer"));
+
+foreach (string p in set)
+{
+    Console.WriteLine(p);
+}
